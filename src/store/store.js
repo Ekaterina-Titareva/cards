de1be561fa66399b3c363e03955a786d0bfc1f73
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-06-06 19:20:19 +0300

    gh-pages

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {cardsSlice} from './cardsSlice'
-
-const store = configureStore({
-    reducer: {
-    cards: cardsSlice.reducer
-    }
-});
-export default store;
